f53b3c49df2520ec877a61616d4b246a7045e05a
flakis05 <localadmin@compozed-tss-05.mobile.asu.edu>
2016-08-01 15:03:16 -0700

    Uses enviormental variables to register

--- .profile.d/self_register.sh
+++ .profile.d/self_register.sh
@@ -1,1 +1,13 @@
-curl 10.144.138.220:8500/v1/agent/service/register -H 'Content-Type: application/json' -X PUT -d '{  "ID": "helloWorld3",  "Name": "helloWorld3",  "api": "127.0.0.1",  "Port": 8080,  "Check": {    "Interval": "10s",    "TTL": "15s"  }}'
+#a='{  "ID": '
+#b=\"${ID}\"
+#c='  ,  "Name": '
+#d=\"${NAME}\"
+#e=' ,  "api": "127.0.0.1",  "Port": 8080,  "Check": {    "Interval": "10s",    "TTL": "15s"  }}'
+#f=$a$b$c$d$e
+
+curl 10.144.138.220:8500/v1/agent/service/register -H 'Content-Type: application/json' -X PUT -d '{  "ID": '\"${ID}\"'  ,  "Name": '\"${NAME}\"' ,  "api": "127.0.0.1",  "Port": 8080,  "Check": {    "Interval": "10s",    "TTL": "15s"  }}'
+
+curl 10.144.138.220:3000/$HELLO
+curl 10.144.138.220:3000/$HELLO
+curl 10.144.138.220:3000/$HELLO
+curl 10.144.138.220:3000/$HELLO
