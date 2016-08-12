764098ff91714da8db57a504aee4c3e5302cb704
localadmin <localadmin@compozed-tss-11.mobile.asu.edu>
2016-08-12 11:43:31 -0700

    Added script pulling env vars

--- .profile.d/self_register.sh
+++ .profile.d/self_register.sh
@@ -1,3 +1,15 @@
+name=$(echo ${VCAP_APPLICATION} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["application_name"]')
+id= echo ${VCAP_APPLICATION} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["application_id"]'
+uris= echo ${VCAP_APPLICATION} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["application_uris"]'
+echo "THE NAME IS:"$name
+echo $id
+echo $uris
+echo '############################################################################################'
+echo 'VCAP:'${VCAP_APPLICATION}
+echo '------------------------------------------------------------------------------------------------'
 
+# curl 10.143.81.111:8500/v1/agent/service/register -H 'Content-Type: application/json' -X PUT -d '{  "ID": '\"${id}\"'  ,  "Name": '\"${name}\"' , "Address": '\"${uris}\"' ,  "Port": 8080,  "Check": {"name": "Health Check", "HTTP": '\"${uris}'/health'\"', "Interval": "10s" }}'
 
-curl 10.144.138.220:8500/v1/agent/service/register -H 'Content-Type: application/json' -X PUT -d '{  "ID": '\"${APPLICATION_ID}\"'  ,  "Name": '\"${APPLICATION_NAME}\"' , "Address": '\"${APPLICATION_URIS}\"' ,  "Port": 8080,  "Check": {"name": "Health Check", "HTTP": '\"${APPLICATION_URIS}\""/health"', "Interval": "10s" }}'
+curl 10.143.81.111:8500/v1/agent/service/register -H 'Content-Type: application/json' -X PUT -d '{  "Name": '\"${name}\"'}'
+
+echo '############################################################################################'
