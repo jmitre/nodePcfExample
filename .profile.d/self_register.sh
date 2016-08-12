54146bd62b8734c0346d7a796e6cab604e05ca33
flakis05 <localadmin@compozed-tss-05.mobile.asu.edu>
2016-08-12 13:34:59 -0700

    added code to reflect health check registration

--- .profile.d/self_register.sh
+++ .profile.d/self_register.sh
@@ -1,15 +1,15 @@
 name=$(echo ${VCAP_APPLICATION} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["application_name"]')
-id= echo ${VCAP_APPLICATION} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["application_id"]'
-uris= echo ${VCAP_APPLICATION} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["application_uris"]'
-echo "THE NAME IS:"$name
-echo $id
-echo $uris
+id=$(echo ${VCAP_APPLICATION} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["application_id"]')
+uris=$(echo ${VCAP_APPLICATION} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["application_uris"][0]')
+echo "THE NAME IS: "$name
+echo "The ID is: "$id
+echo "The uris is: "$uris
 echo '############################################################################################'
 echo 'VCAP:'${VCAP_APPLICATION}
 echo '------------------------------------------------------------------------------------------------'
 
 # curl 10.143.81.111:8500/v1/agent/service/register -H 'Content-Type: application/json' -X PUT -d '{  "ID": '\"${id}\"'  ,  "Name": '\"${name}\"' , "Address": '\"${uris}\"' ,  "Port": 8080,  "Check": {"name": "Health Check", "HTTP": '\"${uris}'/health'\"', "Interval": "10s" }}'
 
-curl 10.143.81.111:8500/v1/agent/service/register -H 'Content-Type: application/json' -X PUT -d '{  "Name": '\"${name}\"'}'
+curl 10.244.68.6:8500/v1/agent/service/register -H 'Content-Type: application/json' -X PUT -d '{"ID": '\"${id}\"'  ,  "Name": '\"${name}\"' , "Address": '\"${uris}\"' ,  "Port": 8080,  "Check": {"name": "Health Check", "HTTP": '\"'http://'${uris}'/health'\"', "Interval": "10s" }}'
 
-echo '############################################################################################'
+echo "#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++================"
